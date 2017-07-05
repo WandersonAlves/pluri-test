daf41930e9bff60c4076fbb38b3f6a418a6bbf2c
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-07-04 22:43:07 -0300

    PROGRESS: Search flex layout complete

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -6,6 +6,21 @@
     function SearchController(AulasService, AulasFactory) {
         var vm = this;
 
+        vm.setClass = function (area) {
+          if (area === "Ciências da Natureza") {
+            return 'green-bg'
+          }
+          else if (area === "Ciências Humanas") {
+            return 'pink-bg';
+          }
+          else if (area === "Linguagens e Códigos") {
+            return 'darkturquoise-bg';
+          }
+          else {
+            return 'red-bg';
+          }
+        }
+
         var _aulasServiceSuccess = function (result) {
             vm.aulas = AulasFactory.filterFromGetAulas(result.data);
             console.log(vm.aulas);
