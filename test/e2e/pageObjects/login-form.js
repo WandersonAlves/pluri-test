d5d5f927718730803f790645883ea1248866f2fc
WandersonAlves <wanderson.alves@accenture.com>
2017-07-05 14:38:50 -0300

    ABORT: Protractor tests removed :angry:

--- test/e2e/pageObjects/login-form.js
+++
@@ -1,9 +0,0 @@
-exports.doLoginSuccess = function() {
-  var email = element(by.id('email-input')),
-      pass = element(by.id('pass-input')),
-      loginBtn = element(by.id('login-btn'));
-
-  email.sendKeys("aluno_1@plurieducacional.com.br");
-  pass.sendKeys(123456);
-  return loginBtn.click();
-};
